00f35feea7d9f3708d9b94795cf6e41aa5fca1bb
Dominion Samuel <theoladejitoluwa@gmail.com>
2026-08-24 08:00:01 +0100

    API working fine through /health endpoint

--- index.ts
+++ index.ts
@@ -10,8 +10,35 @@ interface Task {
   done: boolean;
 }
 
+const tasks: Task[] = [
+  {
+    id: 1,
+    title: "write function",
+    done: false,
+  },
+  {
+    id: 2,
+    title: "connect project to git",
+    done: false,
+  },
+  {
+    id: 1,
+    title: "learn NestJS",
+    done: false,
+  },
+];
+
+app.get("/", (req: Request, res: Response) => {
+  res
+    .status(200)
+    .json({ name: "Task API", version: "1.0", enpionts: "[/tasks]" });
+});
+
+app.get("/health", (req: Request, res: Response) => {
+  res.status(200).json({ status: "ok" });
+});
+
 app.get("/tasks", (req: Request, res: Response) => {
-  const tasks: Task[] = [];
   res.status(200).json({ message: "Sucesfully fetched task", tasks });
 });
 
@@ -41,7 +68,40 @@ app.get("/tasks/:id", (req: Request<TaskParams>, res: Response) => {
   });
 });
 
-app.put("/tasks");
+interface UpdateTaskBody {
+  title?: string;
+  done?: boolean;
+}
+
+app.put(
+  "/tasks/:id",
+  (req: Request<TaskParams, {}, UpdateTaskBody>, res: Response) => {
+    const tasksId = parseInt(req.params.id);
+    const { title, done } = req.body;
+
+    if (title === undefined && done === undefined) {
+      res.status(400).json({ error: "Empty or Invalid body " });
+      return;
+    }
+
+    const taskIndex = tasks.findIndex((t) => t.id === tasksId);
+
+    if (taskIndex === -1) {
+      res.status(400).json({ error: `Task ${tasksId} not found` });
+    }
+
+    if (title !== undefined) {
+      tasks[taskIndex].title = title;
+    }
+    if (done !== undefined) {
+      tasks[taskIndex].done = done;
+    }
+
+    res
+      .status(200)
+      .json({ message: "Task updated sucessfully", task: tasks[taskIndex] });
+  },
+);
 
 const PORT = 3000;
 app.listen(PORT, () => {
